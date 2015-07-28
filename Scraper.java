60ed2b6ebbaa9d8940198544e3335afa9ca3c3dc
shaungeorge <shaungeorgeonline@gmail.com>
2015-07-28 23:04:52 +0100

    Now iterates through the search result pages untill the last page

--- Scraper.java
+++ Scraper.java
@@ -11,15 +11,17 @@ public class Scraper
 	private String root;
 	private Document doc;
 	private Elements list;
+	private int searchResults;
 
 
 	public Scraper(String tag)
 	{
 		this.tag = tag;
-		root = "http://imgur.com/search/score/all/page/1?scrolled&q=";
+		this.root = "http://imgur.com/search/score/all/page/1?scrolled&q=";
 		try {
 			this.doc = Jsoup.connect(root + tag).get(); 
 			this.list = doc.select(".image-list-link img[src$=.jpg]");
+			this.searchResults = 0;
 		} catch (IOException e) {
 			System.out.println(e);
 		}
@@ -33,18 +35,38 @@ public class Scraper
 		String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
 
 		int numberOfImages = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
-		int imagesFound = 0;
+		int imagesFound = 0;	
 
-		System.out.println(imagesFound);
+		int pageCount = 1;	
 
-		for (Element e : list)
+		while (numberOfImages > 0) 
 		{
-			if (imagesFound < numberOfImages) {
-				System.out.println(e.attr("src").substring(2, e.attr("src").length()));
+			for (Element e : list)
+			{
+				//System.out.println(e.attr("src").substring(2, e.attr("src").length()));
 				imagesFound++;
-
 			}
+
+			this.root = "http://imgur.com/search/score/all/page/" + Integer.toString(++pageCount) + "?scrolled&q=";
+			try {
+				this.doc = Jsoup.connect(root + tag).get(); 
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+
+			numberOfImagesSearch = doc.getElementById("searching").text();
+			numberOfImagesSearchList = numberOfImagesSearch.split(" ");
+
+			System.out.println(root + tag);
+			System.out.println(numberOfImages);
+			System.out.println("Subtracting.." + imagesFound);
+
+			numberOfImages -= imagesFound;
+
+			imagesFound = 0;
+
 		}
+
 	}
 
 }
