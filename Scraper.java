b2712a93c178f116ea89cc8438b5719668634098
shaungeorge <shaungeorgeonline@gmail.com>
2015-07-29 19:46:01 +0100

    Downloads images and categorizes by search terms

--- Scraper.java
+++ Scraper.java
@@ -2,34 +2,36 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
-public class Scraper
-{
+
+public class Scraper {
 	private String tag;
 	private String root;
 	private Document doc;
 	private Elements list;
-	private int searchResults;
 
 
-	public Scraper(String tag)
-	{
+	public Scraper(String tag) {
 		this.tag = tag;
-		this.root = "http://imgur.com/search/score/all/page/1154?scrolled&q=";
+		this.root = "http://imgur.com/search/score/all/page/1?scrolled&q=";
+
 		try {
 			this.doc = Jsoup.connect(root + tag).get(); 
 			this.list = doc.select(".image-list-link img[src$=.jpg]");
-			this.searchResults = 0;
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 
 	}
 
-	public void getList()
-	{
+	public void getList() {
+
+
 
 		String numberOfImagesSearch = doc.getElementById("searching").text();
 		String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
@@ -37,24 +39,38 @@ public class Scraper
 		int numberOfImagesTotal = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
 		int numberOfImagesRemaining = numberOfImagesTotal;
 		int currentImagesFound = Integer.parseInt(numberOfImagesSearchList[5]);
-		int imagesFound = 0;	
+		int imagesFound = 0;
+		int imageId = 0;	
 
 		int pageCount = 1;	
 
-		while (!(currentImagesFound == numberOfImagesTotal)) 
-		{
-			System.out.print("New page ");
+		File dir = new File(System.getProperty("user.dir") + "/" + tag);
+		dir.mkdir();
+
+
+		while (!(currentImagesFound == numberOfImagesTotal)) {
+			System.out.print("Downloading page ");
 			System.out.println(pageCount);
 	
-			for (Element e : list)
-			{
-				System.out.println(e.attr("src").substring(2, e.attr("src").length()));
+			for (Element e : list) {
+
+				String url = "http://" + e.attr("src").substring(2, e.attr("src").length() - 5) + ".jpg";
+
+				System.out.println("Downloading image " + url);
+
+				System.out.println(System.getProperty("user.dir"));
+
+				fileDownloader(url, Integer.toString(imageId));
+
+				imageId++;
 				imagesFound++;
 			}
 
 			this.root = "http://imgur.com/search/score/all/page/" + Integer.toString(++pageCount) + "?scrolled&q=";
+
 			try {
-				this.doc = Jsoup.connect(root + tag).get(); 
+				this.doc = Jsoup.connect(root + tag).get();
+				this.list = doc.select(".image-list-link img[src$=.jpg]");
 			} catch (IOException e) {
 				System.out.println(e);
 			}
@@ -69,6 +85,17 @@ public class Scraper
 
 		}
 
+	}
+
+	private void fileDownloader(String url_id, String imageId) {
+		try {
+			URL url = new URL(url_id);
+			BufferedImage image = ImageIO.read(url);
+	        ImageIO.write(image, "jpg",new File(System.getProperty("user.dir") + "/" + tag + "/image" + imageId));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
