3a7d5d4174765292be069f9cd21b70267b7dc95a
shaungeorge <shaungeorgeonline@gmail.com>
2015-08-01 00:51:16 +0100

    Now GUI takes input and sends it to the scrapper class

--- ScraperFrame.java
+++ ScraperFrame.java
@@ -3,28 +3,47 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.TextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
-public class ScraperFrame {
+public class ScraperFrame extends JFrame implements ActionListener {
+
+	private BorderLayout layout;
+	private JButton button1;
+	private JLabel instruction;
+	private TextField tf;
+
+	public ScraperFrame() {
+		super("Imgur scraper");
+
+		layout = new BorderLayout();
+		button1 = new JButton("Download");
+		instruction = new JLabel("Please enter query terms");
+		tf = new TextField(50);
+
+		this.setSize(800, 100);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		add(instruction, BorderLayout.NORTH);
+		add(tf, BorderLayout.WEST);
+		add(button1, BorderLayout.EAST);
+
+		button1.addActionListener(this);
+		this.add(button1);
+	} 
 
 	public static void main(String[] args) {
-
-
-	
-		JFrame jf = new JFrame("Imgur image scraper");
-		JLabel instruction = new JLabel("Enter search query");
-		TextField tf = new TextField(5);
-		JButton download = new JButton("Download");
-
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setSize(400, 100);
-		jf.add(instruction, BorderLayout.NORTH);
-		jf.add(tf, BorderLayout.SOUTH);
-		jf.add(download, BorderLayout.EAST);
-		jf.setVisible(true);
-
-
-
+		ScraperFrame window = new ScraperFrame();
 
 	}
+
+	public void actionPerformed(ActionEvent e) {
+		Scraper scraper = new Scraper(tf.getText());
+		scraper.getList();
+
+	}
+
+
 }
