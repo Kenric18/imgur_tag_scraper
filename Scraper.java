2085e567fd9f66a775c329a889d9dd2fe721367b
shaungeorge <shaungeorgeonline@gmail.com>
2015-08-05 22:03:18 +0100

    started refactoring the code

--- Scraper.java
+++ Scraper.java
@@ -17,137 +17,144 @@ import java.io.File;
 
 
 public class Scraper {
-	private String tag;
-	private String root;
-	private Document doc;
-	private Elements list;
-	private int numberOfImagesTotal;
-	private static volatile boolean running = true;
+    private String tag;
+    private String root;
+    private Document doc;
+    private Elements list;
+    private int numberOfImagesTotal;
+    private static volatile boolean running = true;
+    private String lastUrl;
+    private int currentImagesFound;
+    private int numberOfImagesRemaining;
 
 
 
-	/**
-	* Constructor which does basic crawling
-	* and retrieves basic information such as
-	* total number of images found.
-	*
-	* @param tag Is the search query provided by the user.
-	*/
-	public Scraper(String tag) {
+    /**
+    * Constructor which does basic crawling
+    * and retrieves basic information such as
+    * total number of images found.
+    *
+    * @param tag Is the search query provided by the user.
+    */
+    public Scraper(String tag) {
 
 
-		this.tag = tag;
-		this.root = "http://imgur.com/search/score/all/page/1?scrolled&q=";
-		this.numberOfImagesTotal = 0;
+        this.tag = tag;
+        this.root = "http://imgur.com/search/score/all/page/1?scrolled&q=";
+        this.numberOfImagesTotal = 0;
+        this.lastUrl = null;
+        this.currentImagesFound = 0;
 
-		try {
-			this.doc = Jsoup.connect(root + tag).get(); 
-			this.list = doc.select(".image-list-link img[src$=.jpg]");
+        try {
+            this.doc = Jsoup.connect(root + tag).get(); 
+            this.list = doc.select(".image-list-link img[src$=.jpg]");
 
-			String numberOfImagesSearch = doc.getElementById("searching").text();
-			String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
-			
-			
-			this.numberOfImagesTotal = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
+            String numberOfImagesSearch = doc.getElementById("searching").text();
+            String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
+            
+            
+            this.numberOfImagesTotal = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
 
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
-	}	
+    }    
 
-	/*
-	* This method iterates through the search result
-	* pages and parses it for image urls. It also creates a folder
-	* for the images.
-	*/
+    /*
+    * This method iterates through the search result
+    * pages and parses it for image urls. It also creates a folder
+    * for the images.
+    */
 
-	public void startDownload() {
+    public void startDownload() {
 
-		String numberOfImagesSearch = doc.getElementById("searching").text();
-		String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
+        String numberOfImagesSearch = doc.getElementById("searching").text();
+        String[] numberOfImagesSearchList = numberOfImagesSearch.split(" ");
 
-		this.numberOfImagesTotal = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
-		int numberOfImagesRemaining = this.numberOfImagesTotal;
-		int currentImagesFound = Integer.parseInt(numberOfImagesSearchList[5]);
-		int imagesFound = 0;
-		int imageId = 0;	
+        this.numberOfImagesTotal = Integer.parseInt(numberOfImagesSearchList[7].substring(0, numberOfImagesSearchList[7].length() - 1));
+        int numberOfImagesRemaining = this.numberOfImagesTotal;
+        int currentImagesFound = Integer.parseInt(numberOfImagesSearchList[5]);
+        int imagesFound = 0;
+        int imageId = 0;    
 
-		int pageCount = 1;	
+        int pageCount = 1;    
 
-		File dir = new File(System.getProperty("user.dir") + "/" + tag);
-		dir.mkdir();
-		
-		while (!(currentImagesFound == numberOfImagesTotal) && Scraper.running) {
-			
-			for (Element e : list) {
+        File dir = new File(System.getProperty("user.dir") + "/" + tag);
+        dir.mkdir();
+                
+        while (!(currentImagesFound == numberOfImagesTotal) && Scraper.running) {
+            
+            for (Element e : list) {
 
-				if (Scraper.running) {
+                if (Scraper.running) {
+                	
+              
+                	lastUrl = "http://" + e.attr("src").substring(2, e.attr("src").length() - 5) + ".jpg";
+                	
 
-					String url = "http://" + e.attr("src").substring(2, e.attr("src").length() - 5) + ".jpg";
+                    System.out.println("Downloading image " + lastUrl);
 
-					System.out.println("Downloading image " + url);
+                    fileDownloader(lastUrl, Integer.toString(imageId));
 
-					fileDownloader(url, Integer.toString(imageId));
+                    imageId++;
+                    imagesFound++;
+                    
+                }
+            }
 
-					imageId++;
-					imagesFound++;
-					
-				}
-			}
+            this.root = "http://imgur.com/search/score/all/page/" + Integer.toString(++pageCount) + "?scrolled&q=";
 
-			this.root = "http://imgur.com/search/score/all/page/" + Integer.toString(++pageCount) + "?scrolled&q=";
+            try {
+                this.doc = Jsoup.connect(root + tag).get();
+                this.list = doc.select(".image-list-link img[src$=.jpg]");
+            } catch (IOException e) {
+                System.out.println(e);
+            }
 
-			try {
-				this.doc = Jsoup.connect(root + tag).get();
-				this.list = doc.select(".image-list-link img[src$=.jpg]");
-			} catch (IOException e) {
-				System.out.println(e);
-			}
+            numberOfImagesSearch = doc.getElementById("searching").text();
+            numberOfImagesSearchList = numberOfImagesSearch.split(" ");
+            numberOfImagesRemaining -= imagesFound;
+            imagesFound = 0;
 
-			numberOfImagesSearch = doc.getElementById("searching").text();
-			numberOfImagesSearchList = numberOfImagesSearch.split(" ");
-			numberOfImagesRemaining -= imagesFound;
-			imagesFound = 0;
+            currentImagesFound = Integer.parseInt(numberOfImagesSearchList[5]);
+            
 
-			currentImagesFound = Integer.parseInt(numberOfImagesSearchList[5]);
-			
+        }
 
-		}
+    }
 
-	}
+    /**
+    * This method recieves the image url from startDownload
+    * and then downloads the images to the folder created earlier.
+    *
+    * @param url_id is the image url.
+    * @param imageId is what the image name is going to be when stored.
+    */
+    private void fileDownloader(String url_id, String imageId) {
+        try {
+            URL url = new URL(url_id);
+            BufferedImage image = ImageIO.read(url);
+            ImageIO.write(image, "jpg", new File(System.getProperty("user.dir") + "/" + tag + "/image" + imageId + ".jpg"));
 
-	/**
-	* This method recieves the image url from startDownload
-	* and then downloads the images to the folder created earlier.
-	*
-	* @param url_id is the image url.
-	* @param imageId is what the image name is going to be when stored.
-	*/
-	private void fileDownloader(String url_id, String imageId) {
-		try {
-			URL url = new URL(url_id);
-			BufferedImage image = ImageIO.read(url);
-	        ImageIO.write(image, "jpg", new File(System.getProperty("user.dir") + "/" + tag + "/image" + imageId + ".jpg"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+    /**
+    * Returns total images found related to query.
+    * @return number of images found.
+    */
+    public String getTotalImages() {
+        return Integer.toString(numberOfImagesTotal);
+    }
 
-	/**
-	* Returns total images found related to query.
-	* @return number of images found.
-	*/
-	public String getTotalImages() {
-		return Integer.toString(numberOfImagesTotal);
-	}
-
-	/**
-	* Used to stop the crawler before it finishes.
-	*/
-	public void setRunningMode(boolean b) {
-		Scraper.running = b;
-	}
+    /**
+    * Used to stop the crawler before it finishes.
+    */
+    public void setRunningMode(boolean b) {
+        Scraper.running = b;
+    }
 
 }
--- ScraperFrame.java
+++ ScraperFrame.java
@@ -22,122 +22,124 @@ import java.lang.Character;
 
 public class ScraperFrame extends JFrame implements ActionListener {
 
-	private JPanel searchArea;
-	private JButton downloadButton;
-	private JButton stopDownload;
-	private JPanel queryInfo;
-	private JTextField searchQuery;
-	private JLabel numberOfImages;
+    private JPanel searchArea;
+    private JButton downloadButton;
+    private JButton stopDownload;
+    private JPanel queryInfo;
+    private JTextField searchQuery;
+    private JLabel numberOfImages;
 
-	/**
-	* The default constructor which
-	* is used to initalize a window.
-	* 
-	*/
-	public ScraperFrame() {
+    /**
+    * The default constructor which
+    * is used to initalize a window.
+    * 
+    */
+    public ScraperFrame() {
 
-		// this. keyword add
+        // this. keyword add
 
-		super("imgur scraper class");
+        super("imgur scraper class");
 
-		setLayout(new BorderLayout());
+        setLayout(new BorderLayout());
 
-		searchArea = new JPanel(new GridLayout(2, 1));
-		queryInfo = new JPanel(new GridLayout(2, 2));
+        searchArea = new JPanel(new GridLayout(2, 1));
+        queryInfo = new JPanel(new GridLayout(2, 2));
 
-		downloadButton = new JButton("Download");
-		stopDownload = new JButton("Stop");
-		searchQuery = new JTextField();
-
-
-		searchArea.add(downloadButton);
-		searchArea.add(stopDownload);
-		queryInfo.add(searchQuery);
-
-		downloadButton.addActionListener(this);
-		stopDownload.addActionListener(this);
-
-		this.add(searchArea, BorderLayout.EAST);
-		this.add(queryInfo, BorderLayout.CENTER);
-
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(500, 100);
-		this.setVisible(true);
+        downloadButton = new JButton("Download");
+        stopDownload = new JButton("Stop");
+        searchQuery = new JTextField();
 
 
-	} 
+        searchArea.add(downloadButton);
+        searchArea.add(stopDownload);
+        queryInfo.add(searchQuery);
 
-	public static void main(String[] args) {
-		ScraperFrame window = new ScraperFrame();
-		window.setVisible(true);
+        downloadButton.addActionListener(this);
+        stopDownload.addActionListener(this);
 
-	}
+        this.add(searchArea, BorderLayout.EAST);
+        this.add(queryInfo, BorderLayout.CENTER);
 
-	/**
-	* When JButton is triggered
-	* the events are handled in this method
-	* Input from the JTextField are sent to the Scraper class
-	* after a new Thread is created.
-	* @param e is the event that was triggered.
-	*/
-	public void actionPerformed(ActionEvent e) {
-		
-		if (searchQuery.getText().length() > 0) {
-			
-			Scraper scraperTool = new Scraper(normalizeText(searchQuery.getText()));
-			
-			if (e.getSource() == downloadButton) {
-				
-				numberOfImages = new JLabel(scraperTool.getTotalImages() + " related images found.");
-				
-				queryInfo.add(numberOfImages);
-				queryInfo.revalidate();
-				queryInfo.repaint();
-
-				Thread scraperClass = new Thread(new Runnable(){
-					public void run() {
-						
-						System.out.println("Download started");
-						scraperTool.startDownload();
-
-					}});
-				scraperClass.start();
-
-			} else if (e.getSource() == stopDownload) {
-
-				scraperTool.setRunningMode(false);
-
-				queryInfo.remove(numberOfImages);
-				queryInfo.revalidate();
-				queryInfo.repaint();
-
-				System.out.println("download has stopped");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500, 100);
+        this.setVisible(true);
 
 
-			}
-			
-		} else {
-			JLabel emptyInvalidQuery = new JLabel("No query entered");
-			queryInfo.add(emptyInvalidQuery);
-			queryInfo.revalidate();
-			queryInfo.repaint();
-		}
-		
-		
-	}
-	
-	private String normalizeText(String input) {
-		
-		StringBuffer normalized_text = new StringBuffer();
-		
-		for (int i = 0; i < input.length(); i++) {
-			if (Character.isLetter(input.charAt(i)) || (Character.isDigit(input.charAt(i))) || Character.isWhitespace(input.charAt(i))) {
-				normalized_text.append(input.charAt(i));
-			}
-		}		
-		
-		return normalized_text.toString();
-	}
+    } 
+
+    public static void main(String[] args) {
+        ScraperFrame window = new ScraperFrame();
+        window.setVisible(true);
+
+    }
+
+    /**
+    * When JButton is triggered
+    * the events are handled in this method
+    * Input from the JTextField are sent to the Scraper class
+    * after a new Thread is created.
+    * @param e is the event that was triggered.
+    */
+    public void actionPerformed(ActionEvent e) {
+        
+        if (searchQuery.getText().length() > 0 && searchQuery.getText() != " ") {
+            
+            System.out.println(searchQuery.getText() == " ");
+            
+            Scraper scraperTool = new Scraper(normalizeText(searchQuery.getText()));
+            
+            if (e.getSource() == downloadButton) {
+                
+                numberOfImages = new JLabel(scraperTool.getTotalImages() + " related images found.");
+                
+                queryInfo.add(numberOfImages);
+                queryInfo.revalidate();
+                queryInfo.repaint();
+
+                Thread scraperClass = new Thread(new Runnable(){
+                    public void run() {
+                        
+                        System.out.println("Download started");
+                        scraperTool.startDownload();
+
+                    }});
+                scraperClass.start();
+
+            } else if (e.getSource() == stopDownload) {
+
+                scraperTool.setRunningMode(false);
+
+                queryInfo.remove(numberOfImages);
+                queryInfo.revalidate();
+                queryInfo.repaint();
+
+                System.out.println("download has stopped");
+
+
+            }
+            
+        } else {
+            JLabel emptyInvalidQuery = new JLabel("No query entered");
+            queryInfo.add(emptyInvalidQuery);
+            queryInfo.revalidate();
+            queryInfo.repaint();
+        }
+        
+        
+    }
+    
+    private String normalizeText(String input) {
+        
+        StringBuffer normalized_text = new StringBuffer();
+        
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isLetter(input.charAt(i)) || (Character.isDigit(input.charAt(i))) || Character.isWhitespace(input.charAt(i))) {
+                normalized_text.append(input.charAt(i));
+            }
+        }        
+        
+        return normalized_text.toString();
+    }
 
 
 
